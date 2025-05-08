fb9a4433e3f19d2b62795a3d42cbd370df3f6522
voczhang <voczhang@tencent.com>
2025-05-08 16:26:10 +0800

    some commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-console.log("%cTypescript is currently in use.", 'color: white; background-color: orange; padding: 2px 5px; border-radius: 2px')
+console.log(
+  "%cTypescript is currently in use.",
+  "color: white; background-color: orange; padding: 2px 5px; border-radius: 2px"
+);
 
-import 'src/css/common.scss';
-import 'src/css/index.scss';
+import "src/css/common.scss";
+import "src/css/index.scss";
 
 // 采用光线步进实现 水滴滴落
-import RayMarching from 'src/js/RayMarching';
+import RayMarching from "src/js/RayMarching";
 const rayMarching = new RayMarching("body", true);
 rayMarching.init();
 
